76780248b52fe2ff22017e654f8dad3fcd14bd4d
dmadhu123 <dulikamadushani1234@gmail.com>
2022-02-02 22:58:56 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,20 @@
+// comments ( //single line or /*multi line comment*/ ) 
+
+// case sensitive
+var myScoure = 100
+var myscore = 150
+
+myScoure
+myscore
+
+// cant start with number
+//var 2one = 230;
+
+//can start with $ or _(underscore)
+var $ = 36
+var _h_ = 68
+
+$
+_h_
+
+// using camelCase variable name is better to you
