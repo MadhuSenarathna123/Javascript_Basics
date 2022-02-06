a52ee929cc1682ab34696dbaac2cff325b1f1a14
dmadhu123 <dulikamadushani1234@gmail.com>
2022-02-06 15:37:20 +0530

    data types updated doc

---
+++ dataTypes_object..js
@@ -0,0 +1,18 @@
+// object = properties + methods OR properties OR methods
+
+// there are 3 types of objects
+
+// 01
+var student = new Object();
+
+// 02
+var student02 = {};
+
+//03
+var student03 = {
+                "firstName":"madhushani",
+                 age:24,
+                std:93257,
+                }
+
+console.log(student03.firstName)
